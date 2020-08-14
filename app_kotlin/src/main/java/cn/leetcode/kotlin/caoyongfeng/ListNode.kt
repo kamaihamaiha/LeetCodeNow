2bad12e597f18260c2e68f57f9fd60236febd2ba
caoyongfeng <yongfeng1209@163.com>
2020-08-14 19:34:38 +0800

    第一题

---
+++ app_kotlin/src/main/java/cn/leetcode/kotlin/caoyongfeng/ListNode.kt
@@ -0,0 +1,13 @@
+package cn.leetcode.kotlin.caoyongfeng
+
+/**
+ * Created by caoyongfeng on 2020/8/14
+ * email: caoyongfeng@123eblog.com
+ */
+class ListNode(var value: Int,var next:ListNode?=null) {
+    fun print() {
+        print(value.toString())
+        print("->")
+        next?.print()
+    }
+}
---
+++ app_kotlin/src/main/java/cn/leetcode/kotlin/caoyongfeng/Main.kt
@@ -0,0 +1,9 @@
+package cn.leetcode.kotlin.caoyongfeng
+
+/**
+ * Created by caoyongfeng on 2020/8/14
+ * email: caoyongfeng@123eblog.com
+ */
+fun main() {
+    Code().code21()
+}
