b5374f579941b28d300fcbc0dc9cdadcad54e9f2
张开旭 <zhangkaixu@uexpo.cn>
2020-08-14 16:12:35 +0800

    简单创建了项目结构.

---
+++ app/src/main/java/cn/leetcode/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.leetcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.leetcode.kk.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
