b8bca8aa8099be648840bd575c96b3355fbb6189
caoyongfeng <yongfeng1209@163.com>
2020-08-16 13:50:07 +0800

    临时存储

--- app_kotlin/src/main/java/cn/leetcode/kotlin/caoyongfeng/Main.kt
+++ app_kotlin/src/main/java/cn/leetcode/kotlin/caoyongfeng/Main.kt
@@ -1,9 +1,12 @@
 package cn.leetcode.kotlin.caoyongfeng
 
+import cn.leetcode.kotlin.caoyongfeng.code.Code
+
 /**
  * Created by caoyongfeng on 2020/8/14
  * email: caoyongfeng@123eblog.com
  */
 fun main() {
-    Code().code21()
+    Code().code2()
+
 }
