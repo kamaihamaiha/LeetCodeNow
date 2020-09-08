f3b28e7e16b28630564e3493cf2c6830c62ad013
张震 <zhangzhen@csdn.net>
2020-09-08 20:20:21 +0800

    283. 移动零 本质就是个冒泡

--- app_kotlin/src/main/java/cn/leetcode/kotlin/melo/lckt/LeetCode283.kt
+++ app_kotlin/src/main/java/cn/leetcode/kotlin/melo/lckt/LeetCode283.kt
@@ -2,6 +2,9 @@ package cn.leetcode.kotlin.melo.lckt
 
 /**
  * 283. 移动零
+ *
+ * 必须在原数组上操作，不能拷贝额外的数组。
+尽量减少操作次数
  */
 object LeetCode283 {
     @JvmStatic
@@ -10,16 +13,28 @@ object LeetCode283 {
     }
 
     /**
+     * 如果不限制原数组
+     */
+    fun moveZeroes2(nums: IntArray): Unit {
+//        if (nums.isNotEmpty()) {
+//            var temp = nums.filter { it != 0 } as ArrayList
+//            nums.forEach { if (it == 0) temp.add(0) }
+//            nums = temp.toIntArray()
+//        }
+
+    }
+
+    /**
      * 冒泡..这么,冒泡,冒0
      */
     fun moveZeroes(nums: IntArray): Unit {
-        if (!(nums == null || nums.size == 0)) {
+        if (nums.isNotEmpty()) {
 
             var temp = nums[0]
             //冒泡排序
             for (i in nums.indices) {
                 for (j in i + 1 until nums.size) {
-                    if (nums[i] == 0) {
+                    if (nums[i] == 0|| nums[j]!=0) {
                         temp = nums[j]
                         nums[j] = nums[i]
                         nums[i] = temp
