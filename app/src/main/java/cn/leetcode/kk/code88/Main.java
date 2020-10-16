0af787115b22bd057fe7e19de4c3822160a65339
张开旭 <zhangkaixu@uexpo.cn>
2020-10-16 15:25:17 +0800

    88. 合并两个有序数组. 完成。

--- app/src/main/java/cn/leetcode/kk/code88/Main.java
+++ app/src/main/java/cn/leetcode/kk/code88/Main.java
@@ -4,12 +4,30 @@ import java.util.Arrays;
 
 /**
  * 88. 合并两个有序数组
- *
  */
 public class Main {
     public static void main(String[] args) {
-        int[] nums1 = new int[]{1,2,4,9,0,0,0,0,0};
-        int[] nums2 = new int[]{2,4,8,12,16};
+        int[] nums1 = new int[]{1, 2, 4, 9, 0, 0, 0, 0, 0};
+        int[] nums2 = new int[]{2, 4, 8, 12, 16};
 
+        int m = 4;
+        int n = 5;
+
+        int[] sortArray = mergedSortArray(nums1, nums2, m, n);
+        for (int el : sortArray) {
+            System.out.println(el);
+        }
+    }
+
+    public static int[] mergedSortArray(int[] array1, int[] array2, int m, int n) {
+        for (int i = 0; i < m + n; i++) {
+            if (i >= m) {
+                array1[i] = array2[i - m];
+            }
+        }
+
+        //对 array1 元素排序. 借助 Java API 的巨人肩膀完成
+        Arrays.sort(array1);
+        return array1;
     }
 }
