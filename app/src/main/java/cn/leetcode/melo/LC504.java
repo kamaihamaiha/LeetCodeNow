086a99ce50e6a778018f9f85dfb5ae8abb1391be
zhangzhen <zhangzhen@csdn.net>
2022-03-07 10:38:37 +0800

    504

--- app/src/main/java/cn/leetcode/melo/LC504.java
+++ app/src/main/java/cn/leetcode/melo/LC504.java
@@ -4,4 +4,30 @@ public class LC504 {
     public static void main(String[] args) {
 
     }
+
+    public String convertToBase7(int num) {
+        if (num == 0) {
+            return "0";
+        }
+        String ans = "";
+        boolean isMinus = false;
+        //先取出正负号
+        if (num < 0) {
+            isMinus = true;
+            num = -num;
+        }
+        StringBuffer digits = new StringBuffer();
+
+        while (num > 0) {
+            digits.append(num % 7);
+            num /= 7;
+        }
+        if (isMinus) {
+            digits.append("-");
+        }
+        ans = digits.reverse().toString();
+        return ans;
+    }
+
+
 }
